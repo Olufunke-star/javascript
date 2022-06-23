1c25590572ca6be2ffe44404380f8f1536fbcfdf
Olufunke <jaladesanwa044@stu.ui.edu.ng>
2022-06-23 20:00:20 +0100

    javascript

---
+++ life.js
@@ -0,0 +1,9 @@
+alert('Hello Jennifer');
+
+
+let name=(" Name: Aladesanwa Jennifer olufunke");
+let height=(" Height : 165.00 cm");
+let country=(" Country: Nigeria");
+
+ alert ( name+ "\n"+ height +"\n" + country)  
+ document.write( name +"<br>" + height +"<br>" + country)

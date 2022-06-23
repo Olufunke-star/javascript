3cf5a104aec02437c9284ab33ebc515822d8e44e
Olufunke-star <105389640+Olufunke-star@users.noreply.github.com>
2022-06-23 20:04:42 +0100

    Update life.js

--- life.js
+++ life.js
@@ -5,5 +5,5 @@ let name=(" Name: Aladesanwa Jennifer olufunke");
 let height=(" Height : 165.00 cm");
 let country=(" Country: Nigeria");
 
- alert ( name+ "\n"+ height +"\n" + country)  
+ alert ( name+ "\n"+ height +"\n" + country);  
  document.write( name +"<br>" + height +"<br>" + country)
